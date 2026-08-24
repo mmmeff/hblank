00629a73adf524ed8f0e0cbf399edef2ff6cc69a
Matt Frey <matthewfrey118@gmail.com>
2026-08-23 20:58:24 -0600

    fix: use compatible release notes preset

--- release.config.mjs
+++ release.config.mjs
@@ -2,14 +2,8 @@ export default {
   branches: ["main"],
   tagFormat: "v${version}",
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      { preset: "conventionalcommits" },
-    ],
-    [
-      "@semantic-release/release-notes-generator",
-      { preset: "conventionalcommits" },
-    ],
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     [
       "@semantic-release/exec",
